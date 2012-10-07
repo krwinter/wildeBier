16a97815f12a6293b780724f595c36c1d31a38a3
Ken Winter <kwinter@brighttalk.com>
2012-10-07 23:59:16 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WildeBier::Application.config.secret_token = '5abc43cc0cf5d40adb4fd847d5b94863f26c7f5984349d82b9df9a51a069aeb7af05b3cd325c2a62659d7ce9949cbe31abc6deb84c88e587b7cef9a32ed60f41'
